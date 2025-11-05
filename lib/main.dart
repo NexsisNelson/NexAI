9722718949a8a179f7563517db733863e845d896
Nexsis Nelson <nexsisnelson@gmail.com>
2025-11-05 21:04:26 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nexsis/home_page.dart' show HomePage;
+import 'package:nexsis/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NexOS',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(backgroundColor: Pallete.whiteColor),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
